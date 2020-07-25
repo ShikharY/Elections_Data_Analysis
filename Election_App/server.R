5a533e059458bb862332b39780bc7c2deb0c4431
Shikhar Yadav <shikhar@u.yale-nus.edu.sg>
2020-07-26 02:12:13 +0530

    Shiny App push

---
+++ Election_App/server.R
@@ -0,0 +1,75 @@
+library(shiny)
+library(rsconnect)
+library(haven)
+library(tidyverse)
+library(ggthemes)
+
+shinyServer(function(input, output){
+  
+
+
+
+elections <-
+  read_dta(
+    "Bhavnani India State Election Dataset v 3.0.dta", encoding = "latin1"
+  )
+
+##looping state wise data
+
+election_split <- split(elections, elections$st_name)
+states_data_list <- list()
+for (n in names(election_split)) {
+  dat <- election_split[[n]]   #Extract the data from the list
+  states_data_list[[n]] <- data.frame(
+    st_name = n,
+    year = dat$year,
+    ac_no = dat$ac_no,
+    ac_name = dat$ac_name,
+    ac_type = dat$ac_type,
+    cand_name = dat$cand_name,
+    cand_sex = dat$cand_sex,
+    partyname = dat$partyname,
+    partyabbre = dat$partyabbre,
+    totvotpoll = dat$totvotpoll,
+    electors = dat$electors
+  )
+  
+}
+
+
+
+dat_ratio <- numeric()
+states_gen_ratio_list <- list()
+gen_ratio_dat <- list()
+for (s in names(states_data_list)) {
+  state_years <- unique(states_data_list[[s]]$year)
+  dat_ratio <- numeric()
+  
+  for (i in 1:length(state_years)) {
+    dat_ratio[i] <-
+      (sum(states_data_list[[s]]$year == state_years[i] &
+            states_data_list[[s]]$cand_sex == 'F') /
+      sum(states_data_list[[s]]$year == state_years[i] &
+            states_data_list[[s]]$cand_sex == 'M'))*100
+    
+  }
+  states_gen_ratio_list[[s]] <- data.frame(year = state_years,
+                                           gen_ratio = dat_ratio,
+                                           st_name = unique(states_data_list[[s]]$st_name))
+}
+
+
+
+states_gen_ratio_data <- do.call(rbind, states_gen_ratio_list)
+
+  output$plot <- renderPlot({
+    states_gen_ratio_data %>% subset(st_name == input$state) %>%
+      ggplot(aes(x = year, y = gen_ratio, group = st_name, col = st_name)) + 
+      coord_cartesian(ylim = c(0, input$y_axis), xlim = c(1977, input$year)) + geom_point(aes(size = gen_ratio,)) + 
+      geom_smooth(se = FALSE) + labs( x = "Year", y = "Gender Ratio (Female Candidates per 100  Male Candidates)") +
+      guides(col=guide_legend("State"),
+             size=guide_legend("Women per 100 Men"))
+  })
+
+} )
+
