3193a51a25df05ce7483312df419edb99d584f71
Shikhar Yadav <shikhar@u.yale-nus.edu.sg>
2020-08-14 22:49:06 +0530

    greater slider functioality (drag range)

--- Election_App/ui.R
+++ Election_App/ui.R
@@ -40,7 +40,7 @@ fluidPage(
     selected = c("West Bengal", "Kerala")
   ),
   sliderInput("y_axis", "Set Y axis", min = 1, max = 100, value = 100 ),
-  sliderInput("year", "Year", min = 1978, max = 2015, value = c(1977,2015), step = 1, sep = "" ),
+  sliderInput("year", "Year", min = 1978, max = 2015, value = c(1977,2015), step = 1, sep = "" , dragRange = TRUE),
    ), 
   mainPanel(
   plotOutput("plot")
